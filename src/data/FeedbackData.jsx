682b0f8b7f276f6672ef9cf4eb23a67b48b50ee7
rilindtasholli <rilindtasholli@gmail.com>
2022-08-20 20:29:00 +0200

    added FeedbackList component & global state

---
+++ src/data/FeedbackData.jsx
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+      id: 1,
+      rating: 10,
+      text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse voluptatibus totam magni nihil consectetur quae.'
+  },
+  {
+      id: 2,
+      rating: 9,
+      text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse voluptatibus totam magni nihil consectetur quae.'
+  },
+  {
+      id: 3,
+      rating: 7,
+      text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse voluptatibus totam magni nihil consectetur quae.'
+  }
+]
+
+export default FeedbackData
