b8ccc5c8ee684d199148380362162978b689c6cc
rilindtasholli <rilindtasholli@gmail.com>
2022-08-21 20:48:47 +0200

    added Delete Feedback functionality

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,10 +1,14 @@
+import { FaTimes } from 'react-icons/fa'
 import PropTypes from 'prop-types'
 import Card from "./shared/Card"
 
-function FeedbackItem({feedback}) {
+function FeedbackItem({feedback, handleDelete}) {
   return (
     <Card>
       <div className="num-display">{feedback.rating}</div>
+      <button onClick={() => handleDelete(feedback.id)} className='close'>
+        <FaTimes color='purple'/>
+      </button>
       <div className="text-display">{feedback.text}</div>
     </Card>
   )
